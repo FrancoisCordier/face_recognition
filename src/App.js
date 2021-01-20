8b46d4b12135bd2bbee1334d81f457c4e71650a2
François Cordier <FrancoisCordier@users.noreply.github.com>
2021-01-20 11:50:13 +0100

    Created links with server side API (sign in, register, entries count)

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const app = new Clarifai.App({
   apiKey: "39acbbc653a04a98b3cae723272c952a",
 });
 
+// Paramètres pour l'arrière plan
 const particlesOptions = {
   particles: {
     number: {
@@ -35,9 +36,35 @@ class App extends Component {
       box: {},
       route: "signin",
       isSignedIn: false,
+      user: {
+        id: "",
+        name: "",
+        email: "",
+        entries: 0,
+        joined: "",
+      },
     };
   }
 
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
+
+  /* Fonction qui permet de localiser le visage détecté par le modèle de ML.
+ Clarifai renvoie un paramètre "bounding_box" qui contient les points d'ancrages 
+ en % de la hauteur ou de la largeur de l'image : 
+  - left_col <=> position du premier point en % de la largeur (le point en haut à gauche du visage)
+  - top_row <=> position du deuxième point en % de la hauteur (le point en haut à droite du visage)
+  - right_col <=> position du troisième point en % de la largeur (le point en bas à droite du visage)
+  - bottom_row <=> position du quatrième point en % de la hauteur (le point en bas à gauche du visage) */
   calculateFaceLocation = (data) => {
     const clarifaiFace =
       data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -64,9 +91,22 @@ class App extends Component {
     this.setState({ imageUrl: this.state.input });
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then((response) =>
-        this.displayFaceBox(this.calculateFaceLocation(response))
-      )
+      .then((response) => {
+        if (response) {
+          fetch("http://localhost:3000/image", {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              id: this.state.user.id,
+            }),
+          })
+            .then((response) => response.json())
+            .then((count) =>
+              this.setState(Object.assign(this.state.user, { entries: count }))
+            );
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
       .catch((err) => console.log(err));
   };
 
@@ -91,7 +131,10 @@ class App extends Component {
         {route === "home" ? (
           <div>
             <Logo />
-            <Rank />
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
             <ImageLinkForm
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
@@ -99,9 +142,12 @@ class App extends Component {
             <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
         ) : this.state.route === "signin" ? (
-          <SignIn onRouteChange={this.onRouteChange} />
+          <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
         ) : (
-          <Register onRouteChange={this.onRouteChange} />
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
         )}
       </div>
     );
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,6 @@ import logo from "./neural.png";
 const Logo = () => {
   return (
     <div className="ma4 mt0">
-     
       <Tilt
         className="Tilt br2 shadow-2"
         options={{ max: 25 }}
